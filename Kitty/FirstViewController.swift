23f726f386ac9238686430adae70056d67bee283
ChengFei <2269192760@qq.com>
2016-05-26 14:07:35 +0800

    Initial Commit

---
+++ Kitty/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kitty
+//
+//  Created by fei on 16/5/26.
+//  Copyright © 2016年 ChengFei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
